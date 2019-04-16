98431ea50b91cdf1a72bf4363c063d7f479702e4
huaiyang <huaiyang@huaiyangdeiMac.local>
2019-04-17 07:50:49 +0800

    Initial commit

---
+++ javaweb/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: huaiyang
+  Date: 2019/4/16
+  Time: 下午3:17
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>javaweb</title>
+</head>
+<body>
+    <h1>Good Idea</h1>
+</body>
+</html>
---
+++ src/com/caohuaiyang/com/Hello.java
@@ -0,0 +1,44 @@
+package com.caohuaiyang.com;
+
+//主方法
+public class Hello {
+
+//    private  static final string name = "hello";
+    /*public static void main(String[] args){
+        System.out.println("hello world");
+        ArrayList list = new ArrayList();
+
+        Date date = new Date();
+
+    }*/
+    public static void main(String[] args) {
+
+        System.out.println();
+        System.out.println("args = [" + args + "]");
+
+        int num = 10;
+        int b = 2;
+
+        System.out.println("num = " + num);
+        System.out.println(b);
+
+        String[] arr = new String[]{"Tom", "Jerry"};
+        for (int i = 0; i < num; i++) {
+
+        }
+
+        for (String s : arr) {
+            System.out.println(s);
+        }
+
+
+        for (int i = 0; i < arr.length; i++) {
+            
+        }
+
+        if (arr == null) {
+            
+        }
+
+    }
+}
